71e9cd14a569d2bb70d9ac2b08850abfa6182ef3
chohyeryung <chohyeryungcho@gmail.com>
2021-05-27 23:01:28 +0900

    #1-5. create comment urls

--- daham/urls.py
+++ daham/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'daham'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:board_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:board_id>/', views.detail, name='detail'),
+    path('comment/create/<int:board_id>/', views.comment_create, name='comment_create'),
 ]
