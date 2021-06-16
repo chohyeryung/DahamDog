8fd94f9a9d4e5d1dbcbe0fa4fda21b34d0a52519
chohyeryung <chohyeryungcho@gmail.com>
2021-06-16 19:34:41 +0900

    #1-9. model 생각 후 수정

--- daham/models.py
+++ daham/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 
 
+# class User(models.Model): 유저 모델
+#     name = models.CharField(max_length=200, verbose_name='이름')
+#     email = models.EmailField(max_length=128, verbose_name='이메일')
+#     password = models.CharField(max_length=64, verbose_name='비밀번호')
+
+
 class Board(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)  #유저
     title = models.CharField(max_length=200, null=True)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)  #유저
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
     content = models.CharField(max_length=200, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
 
-class Application(models.Model):  # 아마도 지원 테이블인듯하다! 유저를 만들고 만들어야할듯
+class Application(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
-    commented = models.DateTimeField(auto_now_add=True)
+
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)  #유저
+    created_date = models.DateTimeField(auto_now_add=True)
