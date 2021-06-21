0febc168fe7b1e2ebdb0510a6d8e018f0e1978c9
chohyeryung <chohyeryungcho@gmail.com>
2021-06-22 02:26:52 +0900

    #3-7. board, comment, application 로직에 user 추가

--- daham/models.py
+++ daham/models.py
@@ -1,22 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# class User(models.Model): 유저 모델
-#     name = models.CharField(max_length=200, verbose_name='이름')
-#     email = models.EmailField(max_length=128, verbose_name='이메일')
-#     password = models.CharField(max_length=64, verbose_name='비밀번호')
-
-
 class Board(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)  #유저
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  #유저
     title = models.CharField(max_length=200, null=True)
     content = models.TextField()
-    # deadline = models.DateTimeField(input_formats=['%Y/%m/%d %H:%M:%S'])
     end_date = models.DateField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)  #유저
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  #유저
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
     content = models.CharField(max_length=200, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
@@ -24,5 +18,5 @@ class Comment(models.Model):
 
 class Application(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)  #유저
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  #유저
     created_date = models.DateTimeField(auto_now_add=True)
--- daham/views.py
+++ daham/views.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from django.contrib.auth.decorators import login_required
+
 now = datetime.now()
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
@@ -7,6 +9,7 @@ from django.utils import timezone
 
 from daham.forms import BoardForm, CommentForm
 from daham.models import Board, Application
+
 
 def index(request):
     page = request.GET.get('page', '1')
@@ -21,11 +24,13 @@ def index(request):
     return render(request, 'daham/board_list.html', context)
 
 
+@login_required(login_url='common:login')
 def board_create(request):
     if request.method == 'POST':
         form = BoardForm(request.POST)
         if form.is_valid():
             board = form.save(commit=False)  # commit=Fasle는 아직 임시저장
+            board.user = request.user
             board.created_date = timezone.now()  # created_date까지 설정한 후
             board.save()  # 진짜 저장
             return redirect('daham:index')
@@ -42,6 +47,7 @@ def detail(request, board_id):
     return render(request, 'daham/board_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def comment_create(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
 
@@ -49,6 +55,7 @@ def comment_create(request, board_id):
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
+            comment.user = request.user
             comment.board = board
             comment.save()
             return redirect('daham:detail', board_id=board.id)
@@ -57,15 +64,10 @@ def comment_create(request, board_id):
     context = {'board': board, 'form': form}
     return render(request, 'daham/board_detail.html', context)
 
-    # board = Board.objects.get(id=board_id)
-    # if request.POST.get('content') != '':
-    #     board.comment_set.create(content=request.POST.get('content'), created_date=timezone.now())
-    #
-    # return redirect('daham:detail', board_id=board.id)
 
 def application_create(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
-    Application.objects.create(board=board, created_date=timezone.now())
+    Application.objects.create(board=board, user=request.user, created_date=timezone.now())
     board.save()
 
     return redirect('daham:index')
