5d025afea15c3f9b181c0c47434d8e534507a7c9
chohyeryung <chohyeryungcho@gmail.com>
2021-06-24 00:33:56 +0900

    #3-10. 복수 static css with block extrahead

--- config/settings.py
+++ config/settings.py
@@ -119,11 +119,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 LOGIN_REDIRECT_URL = '/'    # 로그인 성공시 갈 url
