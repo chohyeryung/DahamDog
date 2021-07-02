a643f21b5a413071a79d137e241ae558586bb74f
chohyeryung <chohyeryungcho@gmail.com>
2021-07-02 13:04:50 +0900

    #5-9. mypage profile 수정 form 커스텀 완료

--- daham/views.py
+++ daham/views.py
@@ -79,7 +79,7 @@ def board_update(request, board_id):
             user = request.user
             profile = Profile.objects.get(user=user)
             board.profile = profile
-            board.modify_date = timezone.now()
+            board.end_date = timezone.now()
             board.image = form.cleaned_data['image']
             board.save()
             return redirect('daham:detail', board_id=board.id)
